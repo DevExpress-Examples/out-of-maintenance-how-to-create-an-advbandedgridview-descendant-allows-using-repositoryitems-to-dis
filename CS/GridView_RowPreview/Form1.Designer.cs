d4baaf76ca9167500cf3ce7dee64916c5b0657d8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GridView_RowPreview/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿// Developer Express Code Central Example:
+// GridView descendant allows using RepositoryItems to display/edit Row Preview
+// 
+// This example shows how to create a GridView
+// (ms-help://MS.VSCC.v90/MS.VSIPCC.v90/DevExpress.NETv9.2/DevExpress.XtraGrid/clsDevExpressXtraGridViewsGridGridViewtopic.htm)
+// descendant, which will allow using a specific repository item for displaying and
+// editing data in a row preview section.
+// 
+// 
+// See Also:
+// <kblink id = "K18341"/>
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2002
+
+// Developer Express Code Central Example:
+// How to create a GridView descendant, which will allow using a specific repository item for displaying and editing data in a row preview section
+// 
+// This example shows how to create a GridView
+// (ms-help://MS.VSCC.v90/MS.VSIPCC.v90/DevExpress.NETv9.2/DevExpress.XtraGrid/clsDevExpressXtraGridViewsGridGridViewtopic.htm)
+// descendant, which will allow using a specific repository item for displaying and
+// editing data in a row preview section.
+// 
+// 
+// See Also:
+// <kblink id = "K18341"/>
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2002
+
+using DevExpress.XtraEditors.Repository;
+namespace GridView_RowPreview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.repositoryItemMemoEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemMemoEdit();
+            this.customGridControl1 = new CustomGrid_PreviewRow.CustomGridControl();
+            this.customGridView1 = new CustomGrid_PreviewRow.CustomGridView();
+            this.gridBand1 = new DevExpress.XtraGrid.Views.BandedGrid.GridBand();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemMemoEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // repositoryItemMemoEdit1
+            // 
+            this.repositoryItemMemoEdit1.Name = "repositoryItemMemoEdit1";
+            // 
+            // customGridControl1
+            // 
+            this.customGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.customGridControl1.MainView = this.customGridView1;
+            this.customGridControl1.Name = "customGridControl1";
+            this.customGridControl1.Size = new System.Drawing.Size(568, 361);
+            this.customGridControl1.TabIndex = 0;
+            this.customGridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.customGridView1});
+            // 
+            // customGridView1
+            // 
+            this.customGridView1.Bands.AddRange(new DevExpress.XtraGrid.Views.BandedGrid.GridBand[] {
+            this.gridBand1});
+            this.customGridView1.GridControl = this.customGridControl1;
+            this.customGridView1.Name = "customGridView1";
+            this.customGridView1.PreviewRowEdit = this.repositoryItemMemoEdit1;
+            // 
+            // gridBand1
+            // 
+            this.gridBand1.Caption = "gridBand1";
+            this.gridBand1.Name = "gridBand1";
+            this.gridBand1.VisibleIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 361);
+            this.Controls.Add(this.customGridControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemMemoEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomGrid_PreviewRow.CustomGridControl customGridControl1;
+        private CustomGrid_PreviewRow.CustomGridView customGridView1;
+        private DevExpress.XtraGrid.Views.BandedGrid.GridBand gridBand1;
+        private RepositoryItemMemoEdit repositoryItemMemoEdit1;
+
+
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to create an AdvBandedGridView descendant allows using RepositoryItems to display/edit Row Preview
+
+
+<p>This example shows how to create an AdvBandedGridView descendant, which will allow using a specific repository item for displaying and editing data in a row preview section.</p><br />
+<p><br />
+See Also:</p><p><a href="https://www.devexpress.com/Support/Center/p/K18341">Using a repository item to display and edit data in a row preview section</a></p>
+
+<br/>
+
+

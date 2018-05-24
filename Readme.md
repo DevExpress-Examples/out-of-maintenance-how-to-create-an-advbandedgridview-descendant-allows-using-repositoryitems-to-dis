7e26ee77648852dcb02df1426cdaf8b34017efef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# How to create an AdvBandedGridView descendant allows using RepositoryItems to display/edit Row Preview
+
+
+<p>This example shows how to create an AdvBandedGridView descendant, which will allow using a specific repository item for displaying and editing data in a row preview section.</p><br />
+<p><br />
+See Also:</p><p><a href="https://www.devexpress.com/Support/Center/p/K18341">Using a repository item to display and edit data in a row preview section</a></p>
+
+
+<h3>Description</h3>
+
+Static&nbsp;GridRowPreviewPainter.PreviewTextIndent property marked internal. To get the value of the property, call the GridRowPreviewPainter's GetPreviewTextIndent method and pass a GridDataRowInfo object into it. The modification is made to support true HiDpi
+
+<br/>
+
+
---
+++ VB/GridView_RowPreview/Program.vb
@@ -0,0 +1,51 @@
+﻿' Developer Express Code Central Example:
+' GridView descendant allows using RepositoryItems to display/edit Row Preview
+' 
+' This example shows how to create a GridView
+' (ms-help://MS.VSCC.v90/MS.VSIPCC.v90/DevExpress.NETv9.2/DevExpress.XtraGrid/clsDevExpressXtraGridViewsGridGridViewtopic.htm)
+' descendant, which will allow using a specific repository item for displaying and
+' editing data in a row preview section.
+' 
+' 
+' See Also:
+' <kblink id = "K18341"/>
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E2002
+
+' Developer Express Code Central Example:
+' How to create a GridView descendant, which will allow using a specific repository item for displaying and editing data in a row preview section
+' 
+' This example shows how to create a GridView
+' (ms-help://MS.VSCC.v90/MS.VSIPCC.v90/DevExpress.NETv9.2/DevExpress.XtraGrid/clsDevExpressXtraGridViewsGridGridViewtopic.htm)
+' descendant, which will allow using a specific repository item for displaying and
+' editing data in a row preview section.
+' 
+' 
+' See Also:
+' <kblink id = "K18341"/>
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E2002
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+Namespace GridView_RowPreview
+    Friend NotInheritable Class Program
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread> _
+        Shared Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Application.Run(New Form1())
+        End Sub
+    End Class
+End Namespace
